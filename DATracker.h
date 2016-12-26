cdfffac5605693baff6b05d2bf2cd3ded1052241
马守磊 <hzmashoulei@corp.netease.com>
2016-12-26 14:36:00 +0800

    导入sdk 库

---
+++ DATracker.h
@@ -0,0 +1,536 @@
+
+#import <Foundation/Foundation.h>
+
+@class DAPeople;
+
+/*
+ This class manages tracking and uploading work through sessions.
+
+ It does provide two APIs,
+    'startTrackerWithAppKey' to configure and start data tracking,
+    'trackEvent' to track whatever you like.
+
+ To use this lib, there are some libs/frameworks you need to include:
+    AdSupport.framework - To enable the ADID support by Apple,
+    CoreTelephony.framework - To get the mobile phone carrier,
+    Security.framework - To encrypt data,
+    SystemConfiguration.framework - To check network type (wifi or cellular),
+    libsqlite3.dylib - To support sqlite,
+    libz.dylib - To compress the data before sending.
+
+ Also, this SDK adopt ARC, make sure your compiler support it.
+ 
+ Version: 1.5
+ */
+@interface DATracker : NSObject
+
+/*
+ @property
+ 
+ @abstract
+ Accessor to the DATracker People API object.
+ */
+@property (atomic, readonly, strong) DAPeople *people;
+
+/*
+ Singleton class which maintains a sharedTracker throughout your application.
+ The class should be accessed within code using following syntax:
+ [[DATracker sharedTracker] someFunction]
+ */
++ (id)sharedTracker;
+
+/*
+ Tracking functionality provided by this lib should be activated by this method.
+ It is strongly recommended that this call should be placed in
+    application:DidFinishLaunching
+
+ @appKey The unique key for each application, you can get it from our web system.
+ @appVersion Current version of the application using this SDK.
+ @appChannel Release channel of the application using this SDK.
+ 
+ Options:
+ @isAutoUpload Set it to NO if you don't want SDK sends data automatically.
+               You should manually call upload() method.
+ @sendOnWifi Set it to YES if you want SDK sends data only when WIFI is on,
+             doing this will delay the server to receive data, do not recommend.
+ @udid Device UDID of Apple or anything you set as an unique identifier.
+         This SDK doesn't collect device UDID due to Apple's policy,
+         but you can still set it manually in this interface.
+         Please be careful, doing this might make your App rejected by AppStore.
+ */
+- (void)startTrackerWithAppKey:(NSString *)appKey appVersion:(NSString *)appVersion
+                    appChannel:(NSString *)appChannel;
+
+- (void)startTrackerWithAppKey:(NSString *)appKey appVersion:(NSString *)appVersion
+                    appChannel:(NSString *)appChannel autoUpload:(BOOL)isAutoUpload;
+- (void)startTrackerWithAppKey:(NSString *)appKey appVersion:(NSString *)appVersion
+                    appChannel:(NSString *)appChannel autoUpload:(BOOL)isAutoUpload
+                    sendOnWifi:(BOOL)sendOnWifi;
+- (void)startTrackerWithAppKey:(NSString *)appKey appVersion:(NSString *)appVersion
+                    appChannel:(NSString *)appChannel autoUpload:(BOOL)isAutoUpload
+                    sendOnWifi:(BOOL)sendOnWifi customUDID:(NSString *)udid;
+
+// Set auto upload status
+- (void)setAutoUploadOn:(BOOL)isAutoUpload;
+// Set send only on wifi status
+- (void)setSendOnWifiOn:(BOOL)sendOnWifi;
+
+/*
+ Enable marketing campaign tracking
+ */
+- (void)enableCampaign;
+
+/*
+ Get device ID
+ Notice: This ID isn't the Apple UDID, 
+    and it might be changed later due to Apple's policy change.
+ */
+- (NSString *)getDeviceId;
+
+/*
+ Set user account
+ */
+- (void)loginUser:(NSString *)userId;
+
+- (void)logoutUser;
+
+/*
+ Set user's location
+ */
+- (void)setLocation:(double)latitude andLongitude:(double)longitude;
+
+/*
+ Track a particular event as having occurred.
+ It might make sense to track button click with the name of the button as eventId
+    or track some important view loading with the name of the view as eventId.
+ Also, customized attributes can be added as key value pairs.
+
+ Please DO NOT use this method to record personally identifiable information.
+ The Best way to use this trackEvent is to create all eventId or customized attributes' names
+    as predefined constants and only use those. This helps keeping eventIds and attributes names
+    in order, which will be of great benefit for you to check on the analytics report.
+ Do not use this method inside loops or any place which gets called frequently,
+    which can cause a lot of data to be stored and uploaded.
+ Please DO NOT use any event id begin with "da", which is reserved by SDK itself.
+ */
+- (void)trackEvent:(NSString *)eventId;
+- (void)trackEvent:(NSString *)eventId withAttributes:(NSDictionary *)attributes;
+/*
+ Events can be categoried and labeled
+ */
+- (void)trackEvent:(NSString *)eventId category:(NSString *)category label:(NSString *)label;
+- (void)trackEvent:(NSString *)eventId category:(NSString *)category label:(NSString *)label withAttributes:(NSDictionary *)attributes;
+ 
+/*
+ If you want to record the cost time of an event
+ */
+- (void)trackEvent:(NSString *)eventId costTime:(int)seconds category:(NSString *)category label:(NSString *)label;
+- (void)trackEvent:(NSString *)eventId costTime:(int)seconds category:(NSString *)category label:(NSString *)label withAttributes:(NSDictionary *)attributes;
+/*
+ If you also want to record location of an event
+ */
+- (void)trackEvent:(NSString *)eventId costTime:(int)seconds latitude:(double)latitude longitude:(double)longitude category:(NSString *)category label:(NSString *)label withAttributes:(NSDictionary *)attributes;
+
+/*
+ Record search activity.
+ @keyword search keyword
+ @searchType in which search this activity happens
+ */
+- (void)trackSearch:(NSString *)keyword in:(NSString *)searchType;
+/*
+ Record share activity.
+ @content share content
+ @from share from
+ @to share desitination
+ */
+- (void)trackShare:(NSString*)content from:(NSString *)from to:(NSString *)to;
+/*
+ Record comment activity.
+ @content comment content
+ @onItem comment on
+ */
+- (void)trackComment:(NSString *)content onItem:(NSString *)onItem;
+/*
+ Record favorite activity.
+ @item favorite on
+ */
+- (void)trackFavorite:(NSString *)item;
+
+/*
+ Record Screen
+ @screenName screen name
+ */
+- (void)trackScreen:(NSString *)screenName;
+
+/*
+ Track an exception occurred in @try @catch block.
+ */
+- (void)trackException:(NSException *)exception;
+- (void)trackExceptionWithCallstack:(NSException *)exception;
+- (void)trackExceptionWithName:(NSString *)name reason:(NSString *)reason
+                 callstack:(NSString *)callstack;
+
+
+/*
+ @method
+ 
+ @abstract
+ Starts a timer that will be stopped and added as a property when a
+ corresponding event is tracked.
+ 
+ @discussion
+ This method is intended to be used in advance of events that have
+ a duration. For example, if a developer were to track an "Image Upload" event
+ she might want to also know how long the upload took. Calling this method
+ before the upload code would implicitly cause the <code>track</code>
+ call to record its duration.
+ 
+ <pre>
+ // begin timing the image upload
+ [DATracker trackTimer:@"Image Upload"];
+ 
+ // upload the image
+ [self uploadImageWithSuccessHandler:^{
+ 
+ // track the event
+ [DATracker trackEvent:@"Image Upload"];
+ }];
+ </pre>
+ 
+ @param event   a string, identical to the name of the event that will be tracked
+ 
+ */
+- (void)trackTimer:(NSString *)eventId;
+
+/*
+ @method
+ 
+ @abstract
+ Clears all current event timers.
+ */
+- (void)clearTrackTimer;
+
+/*
+ * Enable crash reporting to track unhandled exceptions and signals
+ */
+- (void)enableCrashReporting;
+
+/*
+ Send all data to server, then remove all data when upload succeeded.
+ All the data collected by this SDK will send to server automatically,
+    Usually, it is NOT necessary to use this method.
+ If you DO want to upload data as soon as possible sometime, be cautious,
+    too many upload tasks will block the queue, might influence other activities,
+ Please use it as less as possible, and at your own risk.
+ */
+- (void)upload;
+
+/*
+ @method
+ 
+ @abstract
+ Set upload timer's interval.
+ Default uploadInterval is 15 seconds.
+ */
+- (void)setUploadInterval:(NSInteger)uploadInterval;
+
+/*
+ @method
+ 
+ @abstract
+ Set upload max bulkSize.
+ Default uploadBulkSize is 100.
+ */
+- (void)setUploadBulkSize:(NSInteger)uploadBulkSize;
+
+/*
+ Track a mission, by marking its began, accomplishment or failurea.
+ @missonId id of mission
+ @reason mission failed reason
+ */
+- (void)trackOnMissionBegan:(NSString *)missionId;
+- (void)trackOnMissionAccomplished:(NSString *)missionId;
+- (void)trackOnMissionFailed:(NSString *)missionId reason:(NSString *)reason;
+
+/*
+ @method
+ 
+ @abstract
+ Returns the currently set super properties.
+ */
+- (NSDictionary *)currentSuperProperties;
+
+/*
+ @method
+ 
+ @abstract
+ Registers super properties, overwriting ones that have already been set.
+ 
+ @discussion
+ Super properties, once registered, are automatically sent as properties for
+ all event tracking calls. They save you having to maintain and add a common
+ set of properties to your events. Property keys must be <code>NSString</code>
+ objects and values must be <code>NSString</code>, <code>NSNumber</code>objects.
+ 
+ @param properties      properties dictionary
+ */
+- (void)registerSuperProperties:(NSDictionary *)properties;
+
+/*
+ @method
+ 
+ @abstract˜
+ Removes a previously registered super property.
+ 
+ @discussion
+ As an alternative to clearing all properties, unregistering specific super
+ properties prevents them from being recorded on future events. This operation
+ does not affect the value of other super properties. Any property name that is
+ not registered is ignored.
+ 
+ Note that after removing a super property, events will show the attribute as
+ having the value <code>undefined</code> in DATracker until a new value is
+ registered.
+ 
+ @param propertyName   array of property name strings to remove
+ */
+- (void)unregisterSuperProperty:(NSString *)propertyName;
+
+/*
+ @method
+ 
+ @abstract
+ Registers super properties without overwriting ones that have already been
+ set.
+ 
+ @discussion
+ Property keys must be <code>NSString</code> objects and values must be
+ <code>NSString</code>, <code>NSNumber</code>, <code>NSNull</code>,
+ <code>NSArray</code>, <code>NSDictionary</code>, <code>NSDate</code> or
+ <code>NSURL</code> objects.
+ 
+ @param properties      properties dictionary
+ */
+- (void)registerSuperPropertiesOnce:(NSDictionary *)properties;
+
+/*
+ @method
+ 
+ @abstract
+ Registers super properties without overwriting ones that have already been set
+ unless the existing value is equal to defaultValue.
+ 
+ @discussion
+ Property keys must be <code>NSString</code> objects and values must be
+ <code>NSString</code>, <code>NSNumber</code>, <code>NSNull</code>,
+ <code>NSArray</code>, <code>NSDictionary</code>, <code>NSDate</code> or
+ <code>NSURL</code> objects.
+ 
+ @param properties      properties dictionary
+ @param defaultValue    overwrite existing properties that have this value
+ */
+- (void)registerSuperPropertiesOnce:(NSDictionary *)properties defaultValue:(id)defaultValue;
+
+///*
+// @method
+// 
+// @abstract
+// Clears all currently set super properties.
+// */
+- (void)clearSuperProperties;
+
+@end
+
+/*
+ @class
+ DA People API.
+ 
+ @abstract
+ Access to the DA People API, available as a property on the main
+ DATracker API.
+ 
+ @discussion
+ <b>You should not instantiate this object yourself.</b> An instance of it will
+ be available as a property of the main DATracker object. Calls to DATracker
+ People methods will look like this:
+ */
+@interface  DAPeople: NSObject
+
+/*
+ DAPeople init
+ 
+ @discussion
+ Called by the property <people> in DATracker.
+ 
+ @param tracker   SDK
+ */
+- (instancetype)initWithSDK:(DATracker *)tracker;
+
+/*
+ set user's location
+ 
+ @param country country
+ @param region  province
+ @param city    city
+ */
+- (void)setLocation:(NSString *)country region:(NSString *)region city:(NSString *)city;
+
+/*
+ set user's account, realName, birthday, gender
+ 
+ @param account
+ @param realName
+ @param birthday
+ @param gender   0 is female, 1 is male, 2 is unknown
+ */
+- (void)setPopulationWithAccount:(NSString *)account realName:(NSString *)realName birthday:(NSDate *)birthday gender:(NSInteger)gender;
+
+/*
+ set user's account
+ 
+ @param account
+ */
+- (void)setAccount:(NSString *)account;
+
+/*
+ set user's real name
+ 
+ @param realName
+ */
+- (void)setRealName:(NSString *)realName;
+
+/*
+ set user's birthday
+ 
+ @param birthday
+ */
+- (void)setBirthday:(NSDate *)birthday;
+
+/*
+ set user's gender
+ 
+ @param gender    0 is female, 1 is male, 2 is unknown
+ */
+- (void)setGender:(NSInteger)gender;
+
+/*
+ @method
+ 
+ @abstract
+ Set properties on the current user in DATracker People.
+ 
+ @discussion
+ The properties will be set on the current user. The keys must be NSString
+ objects and the values should be NSString, NSNumber objects.
+ 
+ @param properties       properties dictionary
+ 
+ */
+- (void)set:(NSDictionary *)properties;
+
+/*
+ @method
+ 
+ @abstract
+ Convenience method for setting a single property in DATracker People.
+ 
+ @discussion
+ Property keys must be <code>NSString</code> objects and values must be
+ <code>NSString</code>, <code>NSNumber</code> objects.
+ 
+ @param property        property name
+ @param object          property value
+ */
+- (void)set:(NSString *)property to:(id)object;
+
+/*
+ @method
+ 
+ @abstract
+ Set properties on the current user in DATracker People, but don't overwrite if
+ there is an existing value.
+ 
+ @discussion
+ This method is identical to <code>set:</code> except it will only set
+ properties that are not already set. It is particularly useful for collecting
+ data about the user's initial experience and source, as well as dates
+ representing the first time something happened.
+ 
+ @param properties       properties dictionary
+ 
+ */
+- (void)setOnce:(NSDictionary *)properties;
+
+/*
+ @method
+ 
+ @abstract
+ Convenience method for setting a single property in DATracker People, but don't overwrite if
+ there is an existing value.
+ 
+ @discussion
+ Property keys must be <code>NSString</code> objects and values must be
+ <code>NSString</code>, <code>NSNumber</code> objects.
+ 
+ @param property        property name
+ @param object          property value
+ */
+- (void)setOnce:(NSString *)property to:(id)object;
+
+/*
+ @method
+ 
+ @abstract
+ Remove property and its value from the current user's profile
+ in DATracker People.
+ 
+ @discussion
+ The property array must ony NSString. For property
+ that don't exist there will be no effect.
+ 
+ @param properties       properties array
+ 
+ */
+- (void)unset:(NSString *)property;
+
+/*
+ @method
+ 
+ @abstract
+ Delete current user's record from DATracker People.
+ */
+- (void)deleteUser;
+
+/*
+ @method
+ 
+ @abstract
+ Track money spent by the current user for revenue analytics.
+ 
+ @param amount          amount of revenue received
+ */
+- (void)trackCharge:(NSNumber *)amount;
+
+/*
+ @method
+ 
+ @abstract
+ Track money spent by the current user for revenue analytics and associate
+ properties with the charge.
+ 
+ @discussion
+ Charge properties allow you segment on types of revenue. For instance, you
+ could record a product ID with each charge so that you could segment on it in
+ revenue analytics to see which products are generating the most revenue.
+ */
+- (void)trackCharge:(NSNumber *)amount withProperties:(NSDictionary *)properties;
+
+/*
+ @method
+ 
+ @abstract
+ Delete current user's revenue history.
+ */
+- (void)clearCharges;
+
+@end
--- README.md
+++ README.md
@@ -1,1 +1,278 @@
-# hubbledata-sdk-ios
+# iOS SDK 使用文档 #
+
+## 依赖库 ##
+
+* AdSupport.framework - 启用 Apple ADID 支持
+* CoreTelephony.framework - 获取运营商
+* Security.framework - 加密支持
+* CoreLocation.framework - 获取定位信息的支持
+* SystemConfiguration.framework - 获取联网方式(wifi, cellular)
+* libsqlite3.dylib - sqlite 支持
+* libz.dylib - gzip 压缩支持
+
+**路径：TARGETS -> Build Phases -> Link Binary With Libraries**
+
+## 导入 SDK ##
+
+将下载包里面 DATracker.h, libMobilytics.a 文件添加到 App 项目中
+
+## 启用 API ##
+
+在 `*AppDelegate.m` `application:didFinishLaunchingWithOptions` 方法中调用如下方法，参数依次为 app key，版本和来源渠道。
+
+    [[DATracker sharedTracker] startTrackerWithAppKey:@"app-key" appVersion:@"0.1" appChannel:@"AppStore"];
+
+如需要禁用 SDK 自动上传数据功能，调用
+
+    [[DATracker sharedTracker] startTrackerWithAppKey:@"app-key" appVersion:@"0.1" appChannel:@"AppStore" autoUpload:NO];
+
+如需要设置只在 wifi 环境下发送数据，调用
+
+    [[DATracker sharedTracker] startTrackerWithAppKey:@"app-key" appVersion:@"0.1" appChannel:@"AppStore" autoUpload:YES sendOnWifi:YES];
+
+**设置为只在 WIFI 下发送数据，会导致服务器接收数据延迟，对统计系统结果的及时性会产生影响，不建议使用**
+
+如需要使用自定义设备标识（比如 UDID），调用
+
+    [[DATracker sharedTracker] startTrackerWithAppKey:@"app-key" appVersion:@"0.1" appChannel:@"AppStore" autoUpload:YES sendOnWifi:NO deviceUDID:@"id-set-by-app"];
+
+**App Key 可从移动分析系统网站获取，不得使用为空值或者 null**
+
+**以下所有调用均需发生在 `启用 API` 之后**
+
+--------------------------
+
+**手动发送数据请调用**
+
+    [[DATracker sharedTracker] upload];
+    
+**设置两次数据发送的最小时间间隔，单位秒**
+
+	- (void)setUploadInterval:(NSInteger)uploadInterval;
+
+**本地缓存的最大事件数目**
+
+	- (void)setUploadBulkSize:(NSInteger)uploadBulkSize;
+
+**手动禁用自动上传**
+
+    [[DATracker sharedTracker] setAutoUploadOn:NO];
+
+**手动开启只在 WIFI 下发送数据**
+
+    [[DATracker sharedTracker] setSendOnWifiOn:YES];
+
+## 推广跟踪 ##
+
+紧跟 `启用 API` 后调用
+
+    [[DATracker sharedTracker] enableCampaign];
+
+## 获取 Device ID ##
+
+    [[DATracker sharedTracker] getDeviceId];
+
+**该 Device ID 并非 Apple UDID, 仅用户系统本身设备去重用途, 并且可能根据 Apple 政策做相应调整, 不保证长期唯一性**
+
+## 用户帐号管理 ##
+
+在用户登录后，请调用如下接口，参数为用户帐号
+
+    - (void)loginUser:(NSString *)userId;
+
+当用户登出后，请调用
+
+    - (void)logoutUser;
+
+**如登录发生在需要捕捉事件后，则该事件无用户信息**
+
+## 用户位置记录 ##
+
+在拿到用户经纬度时, 调用如下接口记录用户位置
+
+    - (void)setLocation:(double)latitude andLongitude:(double)longitude
+
+## 事件捕捉 ##
+
+调用如下方法进行事件捕捉
+
+    - (void)trackEvent:(NSString *)eventId;
+    - (void)trackEvent:(NSString *)eventId withAttributes:(NSDictionary *)attributes;
+
+eventId 为事件标识，如 "login", "buy"
+
+    [[DATracker sharedTracker] trackEvent:@"buy"];
+
+attributes 为自定义字段名称，格式如 "{@"money":@"100", @"timestamp":@"1357872572"}"
+
+可对事件发生时的其他信息进行记录
+
+    [[DATracker sharedTracker] trackEvent:@"login"
+	    withAttributes:[NSDictionary dictionaryWithObjectsAndKeys:@"100", @"money", nil]];
+
+还可以对事件进行归类和打标签
+
+    - (void)trackEvent:(NSString *)eventId cagtegory:(NSString *)category label:(NSString *)label;
+    - (void)trackEvent:(NSString *)eventId cagtegory:(NSString *)category label:(NSString *)label withAttributes:(NSDictionary *)attributes;
+
+如果需要记录事件发生持续时间，可调用如下接口
+
+    - (void)trackEvent:(NSString *)eventId costTime:(int)seconds category:(NSString *)category label:(NSString *)label;
+    - (void)trackEvent:(NSString *)eventId costTime:(int)seconds category:(NSString *)category label:(NSString *)label withAttributes:(NSDictionary *)attributes;
+
+如果需要记录事件发生时的位置信息, 可调用如下接口
+
+    - (void)trackEvent:(NSString *)eventId costTime:(int)seconds latitude:(double)latitude longitude:(double)longitude category:(NSString *)category label:(NSString *)label withAttributes:(NSDictionary *)attributes;
+
+**事件捕捉调用虽然可以使用在任何地方，但最好不要在较多循环内或者非主线程中调用，以及最好不要使用很长 eventID 或者 key value 值，否则会增加 SDK 发送的数据量**
+
+## 事件自定义通用属性 ##
+
+特别地，如果某个事件的属性，在所有事件中都会出现，可以将该属性设置为事件通用属性，通用属性会保存在 App 本地，可调用如下接口：
+
+	- (void)registerSuperProperties:(NSDictionary *)properties;
+
+成功设置事件通用属性后，再通过 trackEvent: 追踪事件时，事件通用属性会被添加进每个事件中。重复调用 registerSuperProperties: 会覆盖之前已设置的通用属性。
+
+如果不覆盖之前已经设定的通用属性（除非已存在的对象值和defaultValue相等），可调用：
+
+	- (void)registerSuperPropertiesOnce:(NSDictionary *)properties;
+	- (void)registerSuperPropertiesOnce:(NSDictionary *)properties defaultValue:(id)defaultValue;
+
+查看当前已设置的通用属性，调用：
+
+	- (NSDictionary *)currentSuperProperties;
+
+删除一个通用属性，调用：
+
+	- (void)unregisterSuperProperty:(NSString *)propertyName;
+
+删除所有已设置的事件通用属性，调用：
+
+	- (void)clearSuperProperties;
+
+**当事件通用属性和事件属性的 Key 冲突时，事件属性优先级最高，它会覆盖事件公共属性。**
+
+## 事件耗时统计 ##
+
+可以通过计时器统计事件的持续时间，默认的时间单位是毫秒。首先，在事件开始时调用 trackTimer: 记录事件开始时间，该方法并不会真正发送事件，接口为
+
+	- (void)trackTimer:(NSString *)eventId;
+
+调用trackEvent: 时，若已记录该事件的开始时间，SDK会在追踪相关事件时自动将事件持续时间记录在事件属性中，并删除该事件定时器。
+
+清除所有的事件定时器，调用：
+
+	- (void)clearTrackTimer;
+
+多次调用 trackTimer:时，相同eventId的事件的开始时间以最后一次调用时为准。
+
+## 异常捕捉 ##
+
+可以在 try catch block 中进行异常捕捉，传入参数为 NSException (含子类)实例
+
+    @try {
+        [@"str" characterAtIndex:10];
+    }
+    @catch (NSException *exception) {
+        [[DATracker sharedTracker] trackException:exception];
+    }
+
+如还需要记录 Callstack，可调用
+
+    [[DATracker sharedTracker] trackExceptionWithCallstack:exception];
+
+如需要手动指定异常信息，可调用
+
+    [[DATracker sharedTracker] trackExceptionWithName:@"exception" reason:@"some reason" callstack:@"long callstack"];
+
+如需要 crash 捕捉, 需要在启用 SDK 后尽早调用如下方法来开启该功能, 并确保 App 中没有绑定过 signal 和 uncaught exception handler
+
+- (void)enableCrashReporting;
+
+## 屏幕 View 捕捉 ##
+
+screenName 为当前 View 名称
+
+    - (void)trackScreen:(NSString *)screenName;
+
+## 搜索动作捕捉 ##
+
+keyword 为搜索关键词，searchType 为搜索类型，比如新闻搜索，视频搜索等
+
+    - (void)trackSearch:(NSString *)keyword in:(NSString *)searchType;
+
+## 分享动作捕捉 ##
+
+content 为分享内容，from 为分享发生地，to 为分享目的地，比如新浪微博，网易微博等
+
+    -(void)trackShare:(NSString*)content from:(NSString *)from to:(NSString *)to;
+
+## 用户任务记录 ##
+
+对用户的任务进行记录，参数为任务 id 和任务失败原因，可用于用户行为完成，用户行为耗时等统计。
+
+    [[DATracker sharedTracker] trackOnMissionBegan:@"mission-1"];
+    [[DATracker sharedTracker] trackOnMissionAccomplished:@"mission-1"];
+    [[DATracker sharedTracker] trackOnMissionFailed:@"mission-2" reason:@"no power"];
+
+## 设置用户属性 ##
+
+为了更准确地提供针对人群的分析服务，可以使用 SDK 的 DATracker的People 设置用户属性。用户可以在留存分析、分布分析等功能中，使用用户属性作为过滤条件，精确分析特定人群的指标。
+
+获取当前用户信息设置实例
+
+	[[DATracker sharedTracker] people];
+
+设置当前用户信息属性（全局或单个）
+
+	- (void)set:(NSDictionary *)profileDict;
+	- (void)set:(NSString *) profile to:(id)content;
+
+设置当前用户信息属性（如果该属性已经设置过，则忽略）。与 set() 方法不同的是，如果被设置的用户属性已存在，则这条记录会被忽略而不会覆盖已有数据，如果属性不存在则会自动创建。因此，setOnce() 比较适用于为用户设置首次激活时间、首次注册时间等属性。
+
+	- (void)setOnce:(NSDictionary *)profileDict;
+	- (void)setOnce:(NSString *) profile to:(id)content;
+
+清除单个用户信息属性
+
+	- (void)unset:(NSString *)property;
+
+删除当前用户记录
+
+	- (void)deleteUser;
+
+记录用户消费接口（用于用户价值评估功能），amount代表金额，其中正数金额代表客户的消费，负数金额代表客户的退款
+
+	- (void)trackCharge:(NSNumber *)amount;
+	- (void)trackCharge:(NSNumber *)amount withProperties:(NSDictionary *)properties;
+
+清除用户的消费记录
+
+	- (void)clearCharges;
+
+## 设置用户公共属性 ##
+
+设置用户账户
+
+    - (void)setAccount:(NSString *)account;
+
+设置用户真实姓名
+
+	- (void)setRealName:(NSString *)realName;
+
+设置用户性别(0-女，1-男，2-未知)
+
+    - (void)setGender:(NSInteger)gender;
+
+设置用户出生日期
+
+    - (void)setBirthday:(NSDate *)birthday;
+    
+批量设置用户基本信息
+
+	- (void)setPopulationWithAccount:(NSString *)account realName:(NSString *)realName birthday:(NSDate *)birthday gender:(NSInteger)gender;
+
+设置用户地址
+
+    - (void)setLocation:(NSString *)country region:(NSString *)region city:(NSString *)city;
